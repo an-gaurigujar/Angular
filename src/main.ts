e985030707ea7c40a7a64849240ace4bc86c83cb
an-gaurigujar <gauri.g@applicationnexus.com>
2025-02-10 12:56:46 +0530

    Add complex-form folder

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,16 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+
+
+//complex-form
+
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { ComplexFormComponent } from './app/complex-form/complex-form.component';
+// import { provideForms } from '@angular/forms';
+
+// bootstrapApplication(ComplexFormComponent, {
+//   providers: [provideForms()]
+// });
